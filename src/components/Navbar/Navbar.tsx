6ae11bbc42cf9d1df6a6b58add35b8f7abe633ab
imran memis <imran1208@hotmail.com>
2022-12-28 10:30:39 +0100

    second commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import SearchBar from './SearchBar'
 import '../../styles/navbar.css'
 import { useNavigate, Link } from 'react-router-dom'
+import {TermContext} from "../../TermContext";
 
 
 const NavBar = () => {
 
     const navigate = useNavigate();
+    const [term, setTerm] = useContext(TermContext);
+
 
 
     return(
@@ -16,7 +19,7 @@ const NavBar = () => {
             <button className="forwardButton" onClick={()=> navigate(1)}>Next</button>
             <button className="forwardSign" onClick={()=> navigate(1)}>&raquo;</button>
             <Link to="/">
-            <button className="home">Home</button>
+            <button className="home" onClick={()=>{setTerm("")}}>Home</button>
             </Link>
             <button className="exit" onClick={()=> navigate(-1)}>X</button>
             <SearchBar/>
